eb2cc5f8c7b6720553d2d38700c4f41e63360d0c
tityocheck <call69@bk.ru>
2020-11-19 16:32:25 +0300

    ICON UPDATED

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,7 +1,7 @@
 import { YMaps, Map, Placemark, ZoomControl } from 'react-yandex-maps';
 
 function MainPage() {
-const cords = [55.684758, 37.738521]
+
 
   return(
         <YMaps>
@@ -10,7 +10,7 @@ const cords = [55.684758, 37.738521]
       <Map style={{width: 1000, height: 800}} defaultState={{ center: [55.684758, 37.738521], zoom: 13
        }}>
         <ZoomControl options={{ float: 'right' }} />
-        <Placemark geometry={cords} options={{iconLayout: 'default#image', iconImageHref: 'https://psv4.userapi.com/c856424/u17278978/docs/d10/aa7b8c36443d/qsymb-unscreen.gif?extra=xp4TcXt4hEbEHagyQRgBy9EbMbpdTWLszKUCVJQ1C1_kTyfQX3vGMvrhQ9LFJCQLaHpwf3zl3UZOxnbIPWv8QkWLZ0HjEe-djBNBRSsLbcPkpVLb_0onm63CXTDLfmjniLmzmYLOA0kHEeDiAl1P_w', iconImageSize: [96, 90], iconImageOffset: [-5, -38]}} />
+        <Placemark geometry={[55.684758, 37.738521]} options={{iconLayout: 'default#image', iconImageHref: 'https://psv4.userapi.com/c856424/u17278978/docs/d10/aa7b8c36443d/qsymb-unscreen.gif?extra=xp4TcXt4hEbEHagyQRgBy9EbMbpdTWLszKUCVJQ1C1_kTyfQX3vGMvrhQ9LFJCQLaHpwf3zl3UZOxnbIPWv8QkWLZ0HjEe-djBNBRSsLbcPkpVLb_0onm63CXTDLfmjniLmzmYLOA0kHEeDiAl1P_w', iconImageSize: [96, 90], iconImageOffset: [-5, -38]}} />
       </Map>
       </div>
   </YMaps>
