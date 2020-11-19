c93524c91ef844e8a377c604cff7b80ac404197d
Evgeniya <evgeniya.ogareva@gmail.com>
2020-11-19 14:17:36 +0300

    Header+signin+signup

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,26 @@
+import style from './style.module.css';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div className={`${style.header}`}>
+      <nav className={`container ${style['header-container']}`}>
+        <ul className={`${style['navigation-list']}`}>
+          <li className={`${style['navigation-list__item']}`}>
+            <Link to='/'>Главная страница </Link>
+          </li>
+
+          <li className={`${style['navigation-list__item']}`}>
+            <Link to='/signin'>Регистрация</Link>
+          </li>
+
+          <li className={`${style['navigation-list__item']}`}>
+            <Link to='/signup'>Войти</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
