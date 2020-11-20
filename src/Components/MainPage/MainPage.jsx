a8135a5c5af1ea245f6f2b8ea5ba1d60bbec069d
tityocheck <call69@bk.ru>
2020-11-20 11:52:44 +0300

    new comins

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -4,7 +4,7 @@ import {getRandomFive, myArray} from '../../helpers/randomFive'
 
 function MainPage() {
   const question = 'https://psv4.userapi.com/c856424/u17278978/docs/d10/aa7b8c36443d/qsymb-unscreen.gif?extra=xp4TcXt4hEbEHagyQRgBy9EbMbpdTWLszKUCVJQ1C1_kTyfQX3vGMvrhQ9LFJCQLaHpwf3zl3UZOxnbIPWv8QkWLZ0HjEe-djBNBRSsLbcPkpVLb_0onm63CXTDLfmjniLmzmYLOA0kHEeDiAl1P_w'
-  const coin = 'https://psv4.userapi.com/c856228/u17278978/docs/d8/bab88fe07f5b/coin-unscreen.gif?extra=T6qR3NtPI4Ak8oXATz8Iga6F09Yjbx9LA08iv1-_fhOgpzCYkoHGBtEVN77fMomh4SGOsEgX8gsjFGxpvZAtL2UZOJjX0gU3QITsVPl0OYZnIpEzUaJyUi6xpWvWJoCCZT8xiI2fqTdKg4VfCL4JlQ'
+  const coin = 'https://psv4.userapi.com/c856224/u17278978/docs/d15/a0d04a1fa2d9/ezgif_com-gif-maker_1.gif?extra=tSnENC1OUQQ6tfX7CS-qFADOz_GQNg-2my74cpBIGk8Tpt-gTltXkAYAwoMPhYz7BFR0rzIqQJXY_Ig9t8NWo8IA4OxJqrKhop8E3DGTV_AF_3BuUvhib75gohD-HQYP-3Rpv71qkWwq_yz5ZrrXMg'
 
 
   const newFive = getRandomFive(myArray)
@@ -31,7 +31,7 @@ console.log(newFive);
                   options={{
                     iconLayout: 'default#image',
                     iconImageHref:
-                      question,
+                      coin,
                     iconImageSize: [96, 90],
                     iconImageOffset: [-5, -38],
                   }}
