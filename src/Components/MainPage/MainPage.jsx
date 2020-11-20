a362b2c47e9d2c79da11962b1dd77b441d7c3d82
Evgeniya <evgeniya.ogareva@gmail.com>
2020-11-20 12:25:06 +0300

    fixes

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,45 +1,34 @@
 import { useState } from 'react';
 import { YMaps, Map, Placemark, ZoomControl } from 'react-yandex-maps';
 import style from './style.module.css';
-
+import { getRandomFive, myArray } from '../../helpers/randomFive';
 import Modal from './Modal';
 import { useHistory } from 'react-router-dom';
 import image from '../../img/5003977.jpeg';
 
+const newFive = getRandomFive(myArray);
+
 function MainPage() {
   const [isOpen, setIsOpen] = useState(false);
   const history = useHistory();
+
+  const question =
+    'https://psv4.userapi.com/c856532/u17278978/docs/d1/ca429eca48f4/new_qsymb.gif?extra=Qig3hBJor4aZYSnBga7yMDgjgyVEwkLHZVlt7CNaRHNFrDfsOcvhIDGsG3-D6RdVlSxALtlNWFXrYWY2N-puzrktvNnrST6lfDvWcJjr0Ostb7D9Gr1vBnXpTVlZLeA6usdwPzkTBjk5uzSInFRYIg';
+  const coin =
+    'https://psv4.userapi.com/c856224/u17278978/docs/d15/a0d04a1fa2d9/ezgif_com-gif-maker_1.gif?extra=tSnENC1OUQQ6tfX7CS-qFADOz_GQNg-2my74cpBIGk8Tpt-gTltXkAYAwoMPhYz7BFR0rzIqQJXY_Ig9t8NWo8IA4OxJqrKhop8E3DGTV_AF_3BuUvhib75gohD-HQYP-3Rpv71qkWwq_yz5ZrrXMg';
   return (
-    <>
-      <YMaps>
-
-import {getRandomFive, myArray} from '../../helpers/randomFive'
-
-function MainPage() {
-  const question = 'https://psv4.userapi.com/c856424/u17278978/docs/d10/aa7b8c36443d/qsymb-unscreen.gif?extra=xp4TcXt4hEbEHagyQRgBy9EbMbpdTWLszKUCVJQ1C1_kTyfQX3vGMvrhQ9LFJCQLaHpwf3zl3UZOxnbIPWv8QkWLZ0HjEe-djBNBRSsLbcPkpVLb_0onm63CXTDLfmjniLmzmYLOA0kHEeDiAl1P_w'
-  const coin = 'https://psv4.userapi.com/c856228/u17278978/docs/d8/bab88fe07f5b/coin-unscreen.gif?extra=T6qR3NtPI4Ak8oXATz8Iga6F09Yjbx9LA08iv1-_fhOgpzCYkoHGBtEVN77fMomh4SGOsEgX8gsjFGxpvZAtL2UZOJjX0gU3QITsVPl0OYZnIpEzUaJyUi6xpWvWJoCCZT8xiI2fqTdKg4VfCL4JlQ'
-
-
-  const newFive = getRandomFive(myArray)
-console.log(newFive);
-  return (
-
     <YMaps>
       <div className={style.map}>
-        My awesome application with maps!
-
-        <div
-          onMouseDown={() => setIsPushed((prev) => !prev)}
-          className={`${style.map}`}
-        >
+        <div className={`${style.map}`}>
           <Map
             className={`${style.map} ${style.myMaps} `}
             defaultState={{
               center: [55.684758, 37.738521],
-              zoom: 12,
+              zoom: 11,
               behaviors: [
                 'drag',
                 'dblClickZoom',
+                'CtrlZoom',
                 'rightMouseButtonMagnifier',
                 'multiTouch',
               ],
@@ -53,30 +42,26 @@ console.log(newFive);
               <img src={image} alt='foto' width='250px' />
             </Modal>
 
-          
             <ZoomControl options={{ float: 'right' }} />
-            {
-              newFive.map((el, i) => (
-                <Placemark
-                                onClick={() => history.push('/signin')}
-              onMouseEnter={() => setIsOpen(() => !isOpen)}
-              onMouseLeave={() => setIsOpen(() => !isOpen)}
+            {newFive.map((el, i) => (
+              <Placemark
+                // onClick={() => setIsOpen(() => !isOpen)}
+                onMouseEnter={() => setIsOpen(() => !isOpen)}
+                onMouseLeave={() => setIsOpen(() => !isOpen)}
                 key={i}
-                  geometry={el}
-                  options={{
-                    iconLayout: 'default#image',
-                    iconImageHref:
-                      coin,
-                    iconImageSize: [96, 90],
-                    iconImageOffset: [-5, -38],
-                  }}
-                />
-              ))
-            }
+                geometry={el}
+                options={{
+                  iconLayout: 'default#image',
+                  iconImageHref: coin,
+                  iconImageSize: [96, 90],
+                  iconImageOffset: [-5, -38],
+                }}
+              />
+            ))}
           </Map>
         </div>
-      </YMaps>
-    </>
+      </div>
+    </YMaps>
   );
 }
 
