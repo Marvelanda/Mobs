c2ec6af9332664643ce25dc172f16af0cddd4c16
Victor <venom621@list.ru>
2020-11-23 13:26:21 +0300

    SIGNinSAGA

--- src/redux/features/sagas/rootSagas.js
+++ src/redux/features/sagas/rootSagas.js
@@ -3,7 +3,7 @@ import { all } from 'redux-saga/effects';
 import { placesWatcher, addNewPlaceWatcher } from './placesSaga';
 import { reviewsWatcher, addReviewWatcher } from './reviewsSaga';
 import { fivePlacesWatcher } from './fivePlacesSaga';
-
+import {userWatcher} from './authSaga'
 export default function* rootSaga() {
   yield all([
     placesWatcher(),
@@ -11,6 +11,7 @@ export default function* rootSaga() {
     fivePlacesWatcher(),
     addNewPlaceWatcher(),
     reviewsWatcher(),
+    userWatcher(),
   ]);
 
   // code after all-effect
