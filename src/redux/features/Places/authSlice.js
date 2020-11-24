284e01672f83ca4651de5874be931732a6ddbac5
Victor <venom621@list.ru>
2020-11-24 10:17:20 +0300

    LEEEEEETSGOOOOOOOOOOOOOOO

--- src/redux/features/Places/authSlice.js
+++ src/redux/features/Places/authSlice.js
@@ -36,10 +36,10 @@ export const getUser = (email, password) => {
   };
 };
 
-export const addUser = (username, email, password, fivePlaces) => {
+export const addUser = (userName, email, password, fivePlaces) => {
   return {
     type: ADDUSER,
-    username,
+    userName,
     email,
     password,
     fivePlaces,
