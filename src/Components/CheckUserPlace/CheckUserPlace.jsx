74a06e928e53555cec367a7e4dbd46b2d2c7ff8d
Evgeniya <evgeniya.ogareva@gmail.com>
2020-11-25 19:22:18 +0300

    fixes

--- src/Components/CheckUserPlace/CheckUserPlace.jsx
+++ src/Components/CheckUserPlace/CheckUserPlace.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkPlaceSaga } from '../../redux/features/Places/placeSlice';
 import { Link } from 'react-router-dom';
 import style from './style.module.css';
-import { checkPlaceOpenModal, setModalClass, setModalCheckPlace } from '../../redux/features/Places/placeSlice'
+import {
+  checkPlaceOpenModal,
+  setModalClass,
+  setModalCheckPlace,
+} from '../../redux/features/Places/placeSlice';
 
-function CheckUserPlace({onClick:onOpenPlaceMessage}) {
-  
+function CheckUserPlace({ onClick: onOpenPlaceMessage }) {
   const dispatch = useDispatch();
-  const user = localStorage.user
-  const message = useSelector((state) => state.places.message)
-
+  const user = localStorage.user;
+  const message = useSelector((state) => state.places.message);
 
   const checkPlace = (message) => {
     console.log('ЧТО ПРОИСХОДИТ???');
-      navigator.geolocation.getCurrentPosition(function(position) {
-       dispatch(checkPlaceSaga(position.coords.latitude, position.coords.longitude, user));
-      });
+    navigator.geolocation.getCurrentPosition(function (position) {
+      dispatch(
+        checkPlaceSaga(
+          position.coords.latitude,
+          position.coords.longitude,
+          user
+        )
+      );
+    });
     dispatch(setModalClass('animate__animated animate__rollIn'));
-    dispatch(checkPlaceOpenModal(true))
+    dispatch(checkPlaceOpenModal(true));
     dispatch(setModalCheckPlace(message));
-  }
-  
- 
+  };
 
   return (
     <div className={style.placeCheckBtn}>
-<<<<<<< HEAD
       <Link onClick={() => checkPlace(message)}>Посетить</Link>
-=======
-      <button onClick={checkPlace}>Засчитать посещение</button>
-  { message && <div className='text'>{message}</div> }
->>>>>>> d12840db759fa04a705947ef910da008674f8cae
     </div>
-  )
+  );
 }
 
-export default CheckUserPlace
+export default CheckUserPlace;
