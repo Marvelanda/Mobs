c94b61b2d55b6346b8b33fc3de9de82229896833
tityocheck <call69@bk.ru>
2020-11-27 12:17:38 +0300

    for friday merge

--- src/Components/DetailedPlace/DetailedPlace.jsx
+++ src/Components/DetailedPlace/DetailedPlace.jsx
@@ -20,11 +20,11 @@ import SwiperCore, {
   EffectCoverflow,
 } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/swiper.scss';
-// import 'swiper/components/navigation/navigation.scss';
-// import 'swiper/components/pagination/pagination.scss';
-// import 'swiper/components/scrollbar/scrollbar.scss';
-// import 'swiper/components/effect-coverflow/effect-coverflow.scss';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+import 'swiper/components/effect-coverflow/effect-coverflow.scss';
 import StarRatingComponent from 'react-star-rating-component';
 import ModalDetails from './ModalDetails';
 
